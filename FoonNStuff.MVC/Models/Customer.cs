fc4c9a7ed37f6c73620fbe37ddecd0fa886842fb
Kyle Davison <kyle.w.davison@gmail.com>
2019-06-25 11:17:49 -0400

    DB Entities setup

---
+++ FoonNStuff.MVC/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoonNStuff.MVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+        //public string FullName {get{return $"{FirstName} {LastName}";}}
+    }
+}
